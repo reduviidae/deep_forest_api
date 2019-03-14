14b28c8cb33367a31eefa628427cfdfbf3dcb825
reduviidae <wiljago@gmail.com>
2019-03-14 12:06:01 -0400

    first commit

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,11 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+  end
+end
