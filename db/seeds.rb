13d9ceb66b95fe1c9906966003757bca99a8bf79
reduviidae <wiljago@gmail.com>
2019-03-19 10:45:01 -0400

    seedfile & serializer updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+Game.destroy_all
+UserGame.destroy_all
+Conversation.destroy_all
+Message.destroy_all
+
+15.times do
+  User.create({name: Faker::GreekPhilosophers.name, pronouns: ["he/him", "she/her", "they/them"].sample, password_digest:'dog'})
+end
 
 5.times do
-  User.create({name: Faker::GreekPhilosophers.name, pronouns: ["he/him", "she/her", "they/them"].sample, password_digest:'dog'})
+  Game.create({title: Faker::BossaNova.song})
+end
+
+User.all.each do |user|
+  UserGame.create({user_id: user.id, game_id: Game.all.sample.id})
+end
+
+Game.all.each do |game|
+  Conversation.create({game_id: game.id})
+end
+
+Conversation.all.each do |conversation|
+  10.times do
+    Message.create({content: Faker::Hacker.say_something_smart, conversation_id: conversation.id, user_id: User.all.sample.id})
+  end
 end
