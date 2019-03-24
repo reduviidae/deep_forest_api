ae0e85e2a8e9388058ebf1072ddbe89bce98c13d
reduviidae <wiljago@gmail.com>
2019-03-24 12:55:54 -0400

    new users added to main game upon creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
+      UserGame.create(user_id: @user.id, game_id: 11)
       @token = encode_token(user_id: @user.id)
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
