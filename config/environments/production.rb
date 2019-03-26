a6383d1df289f5cef9485e2f9702f9f102120c8f
reduviidae <wiljago@gmail.com>
2019-03-25 20:26:13 -0400

    local server demo working 100% mvp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_cable.url = "ws://localhost:3000/cable"
+  config.action_cable.url = "ws://localhost:3334/cable"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
